ffcaba765f660af9b8397c160a65b7825f985f49
unknown <SWen@vcswen7.Paypros.com>
2014-09-29 15:36:56 -0700

    init commit

---
+++ web-based-restful-prject/web-based-restful-prject/src/main/java/com/web/restful/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.web.restful;
+
+/**
+ * Created by SWen on 9/29/2014.
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("main class");
+    }
+}
